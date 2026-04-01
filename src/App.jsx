dd240eb7242c7c34d625b6e72cb685be905d49de
Tanishka <tanishkapundhir8@gmail.com>
2026-04-01 22:35:49 +0530

    Update the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <h1 className="text-5xl text-center mt-20">
-      Tailwind Working 🚀
-    </h1>
+    <div className="bg-black text-white">
+      <Navbar />
+      <Hero />
+    </div>
   );
 }
 
