93f0c5a8d6f69b674bc7e2722b0fcbdbda4e0d2f
Tanishka <tanishkapundhir8@gmail.com>
2026-04-04 22:24:48 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import TechnicalWriting from "./components/TechnicalWriting";
-
+import Certifications from "./components/Certifications";
 function App() {
   return (
     <div className="bg-[#0f0f0f] text-white min-h-screen">
@@ -12,6 +12,7 @@ function App() {
       <About />
       <Projects />
       <TechnicalWriting />
+      <Certifications />
     </div>
   );
 }
