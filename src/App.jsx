0077c108acd666b128f2312d9d8d2c57cca54752
Tanishka <tanishkapundhir8@gmail.com>
2026-04-04 20:16:57 +0530

    Updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="bg-black text-white">
+    <div className="bg-[#0f0f0f] text-white">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
